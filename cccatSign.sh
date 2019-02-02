dec34c7acebaf020db98dfaa019f2bd8685b0ac7
Saszr <saszrlsh@gmail.com>
2019-02-02 16:57:56 +0800

    解决签到成功流量消息不提示
    linux shell下16进制 “\uxxxx” Unicode to UTF-8中文 -> 使用ascii2uni命名转换 -> \u4e86337MB不能识别转换 -> shell字符串处理

--- cccatSign.sh
+++ cccatSign.sh
@@ -5,8 +5,13 @@ curl -d "email=xxx&passwd=xxx&remember_me=week" -c cccat_cookie.txt https://ccca
 
 response=$(curl -b cccat_cookie.txt https://cccat.cc/user/_checkin.php)
 
-re=$(echo -en $response)
 
-# tg机器人消息通知
+
+# tg机器人消息通知  -------无需求以下代码请删除-------
+
+#返回消息格式化
+str0=${response#*:}&&str1=${str0%"}"*}&&str2=${str1/\u4e86/\u4e86 }&&str3=${str2/\u00d7/\u00d7 }
+str=$(echo $str3 |ascii2uni -a U -q)
+
 # 个人id替换 %uid（@userinfobot），bot_token替换 %bot_token（@BotFather）
-curl -d "chat_id=%uid&parse_mode=Markdown&text=#CCCAT签到%0A返回结果：$re" https://api.telegram.org/bot%bot_token/sendMessage
+curl -d "chat_id=%uid&parse_mode=Markdown&text=#CCCAT签到%0A$str" https://api.telegram.org/bot%bot_token/sendMessage
