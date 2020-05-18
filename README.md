9bf2dc2e09741f32ce1e953d6c0f221735a36984
Saszr <saszrlsh@gmail.com>
2020-05-18 21:15:39 +0800

    🌟 fix: 绕过 hCaptcha 验证

--- README.md
+++ README.md
@@ -1,26 +1,43 @@
 # Automation
+
 lazy~
 
-## cccatSign.sh（cccat自动签到
-    telegram bot🤖提醒；多用户
-食用方法：阅读相关代码注释->上传服务器crontab定时任务
-```
+## cccatSign.sh（cccat 自动签到
+
+- 🌟 5.17 cccat.io 启用 hCaptcha 登陆验证
+
+---
+
+telegram bot🤖 提醒；多用户
+
+食用方法：阅读相关代码注释->上传服务器 crontab 定时任务
+
+```shell
 crontab -e
 
 #添加
 0 6 * * * /root/cccatSign.sh >/dev/null 2>&1
 ```
-    0 6 * * *               6点（例如 30 5 * * * 为5点30 ）（艹艹猫4点签到刷新）
-    /root/cccatSign.sh      文件地址
-    >/dev/null 2>&1         不发送日志
-ps.宝塔计划任务有bug，自己crontab，注意文件权限
-## ShadowsocksServerPing.sh
-    计划用于路由器Padavan检测ss服务端是否宕机，配合ServerChan进行微信消息推送
-食用方法：阅读相关代码注释->上传服务器crontab定时任务
+
+```shell
+0 6 * * *               6点（例如 30 5 * * * 为5点30 ）（艹艹猫4点签到刷新）
+/root/cccatSign.sh      文件地址
+>/dev/null 2>&1         不发送日志
 ```
+
+ps.宝塔计划任务有 bug，自己 crontab，注意文件权限
+
+## ShadowsocksServerPing.sh
+
+计划用于路由器 Padavan 检测 ss 服务端是否宕机，配合 ServerChan 进行微信消息推送
+
+食用方法：阅读相关代码注释->上传服务器 crontab 定时任务
+
+```shell
 crontab -e
 
 #添加
 0/30 * * * * /root/ShadowsocksServerPing.sh >/dev/null 2>&1
 ```
+
 ps.注意文件权限，允许执行 744
--- cccatSign.sh
+++ cccatSign.sh
@@ -1,40 +1,32 @@
 #!/bin/bash
 
 # 注意帐号，密码替换
-curl -d "email=xxx&passwd=xxx&remember_me=week" -c cccat_cookie.txt https://cccat.cc/user/_login.php
-
-response=$(curl -b cccat_cookie.txt https://cccat.cc/user/_checkin.php)
-
+curl --cookie "uid=xxx;user_email=xxx;user_pwd=xxx" https://cccat.io/user/_checkin.php
 
 # 以上 无通知单用户
 # 以下 tg机器人消息通知；可多账号
 # 选择使用版本，删除其他代码
+# 🌟 uid/user_email/user_pwd 需要登录后查看网站的 Cookis，f12->Application 里查看或者使用相关插件 http://www.editthiscookie.com/
 
 #!/bin/bash
 
 # 注意帐号，密码替换
-user1="email=xxx&passwd=xxx&remember_me=week"
-#user2="email=xxx&passwd=xxx&remember_me=week"
-#user3="email=xxx&passwd=xxx&remember_me=week"
-users="$user1 $user2 $user3"
-set -- $users
-while [ -n "$1" ]; do
-        #tg机器人消息通知 用户名格式化
-        username1=${1#*"="}&&username2=${username1%%"&"*}
-        #帐号密码的判断，防止直接使用之前本地cookie导致消息通知错误
-        login=$(curl -d "$1" -c cccat_cookie.txt https://cccat.cc/user/_login.php)
-        code="ok"
-        if [[ $login == *$code* ]]
-        then
-            response=$(curl -b cccat_cookie.txt https://cccat.cc/user/_checkin.php)
-            #返回消息格式化
-            str0=${response#*:}&&str1=${str0%"}"*}&&str2=${str1/\u4e86/\u4e86 }&&str3=${str2/\u00d7/\u00d7 }
-            str=$(echo $str3 |ascii2uni -a U -q)
-            # 个人id替换 %uid（@userinfobot），bot_token替换 %bot_token（@BotFather）
-            curl -d "chat_id=%uid&text=#CCCAT签到%0A$username2%0A$str" https://api.telegram.org/bot%bot_token/sendMessage
-        else
-            # 个人id替换 %uid（@userinfobot），bot_token替换 %bot_token（@BotFather）
-            curl -d "chat_id=%uid&text=#CCCAT签到%0A$username2%0A登录失败!" https://api.telegram.org/bot%bot_token/sendMessage
-        fi
-    shift
+user1="uid=xxx;user_email=xxx;user_pwd=xxx"
+#user2="uid=xxx;user_email=xxx;user_pwd=xxx
+#user3="uid=xxx;user_email=xxx;user_pwd=xxx"
+
+users=($user1)
+# users=($user1 $user2 $user3)
+
+for i in "${!users[@]}"; do
+  #tg机器人消息通知 用户名格式化
+  userAllInfo=${users[$i]}
+  a=${userAllInfo#*"user_email="} && userEmail=${a%%";user_pwd"*}
+  #绕过hCaptcha登录验证
+  response=$(curl --cookie "${users[$i]}" https://cccat.io/user/_checkin.php)
+  #返回消息格式化
+  str0=${response#*:} && str1=${str0%"}"*} && str2=${str1/\u4e86/\u4e86 } && str3=${str2/\u00d7/\u00d7 }
+  str=$(echo $str3 | ascii2uni -a U -q)
+  # 个人id替换 %uid（@userinfobot），bot_token替换 %bot_token（@BotFather）
+  curl -d "chat_id=519099094&text=#CCCAT签到%0A$userEmail%0A$str" https://api.telegram.org/bot%bot_token/sendMessage
 done
